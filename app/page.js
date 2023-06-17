4744fe0efb514a1b47e503fe65a15c8c079749f6
Michael <lemichael1215@gmail.com>
2023-06-17 21:49:23 +0300

    added Link

--- app/page.js
+++ app/page.js
@@ -1,7 +1,14 @@
+import Link from "next/link";
+
 export const metadata = {
   title: "NextJS",
 };
 
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  return (
+    <>
+      <h1>Hello, Next.js!</h1>
+      <Link href="/dashboard">Dashboard</Link>
+    </>
+  );
 }
